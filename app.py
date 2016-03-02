ef1b3cddcdc4078ba5fde555bcd808ef2c81a316
=joejoe <=jozefchmelar@gmail.com>
2016-03-02 10:27:48 +0100

    kooo

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ This file creates your application.
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import BigInteger, Column, ForeignKey, Integer, String, Table, Text, text
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
 
@@ -18,17 +21,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-    def __repr__(self):
-        return '<Name %r>' % self.name
 
 ###
 # Routing for your application.
@@ -77,3 +69,55 @@ def page_not_found(error):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Company(Base):
+    __tablename__ = 'Company'
+
+    idCompany = Column(BigInteger, primary_key=True)
+    name = Column(String(30))
+
+
+class Project(Base):
+    __tablename__ = 'Projects'
+
+    idProject = Column(BigInteger, primary_key=True)
+    number = Column(String(20), nullable=False)
+    message = Column(Text)
+    idCompany = Column(ForeignKey(u'Company.idCompany'), nullable=False, index=True)
+    name = Column(String(30))
+    comment = Column(String(200))
+
+    Company = relationship(u'Company')
+    Users = relationship(u'User', secondary='UserHasProject')
+
+
+t_UserHasProject = Table(
+    'UserHasProject', metadata,
+    Column('idUser', ForeignKey(u'Users.idUser'), primary_key=True, nullable=False),
+    Column('idProject', ForeignKey(u'Projects.idProject'), primary_key=True, nullable=False)
+)
+
+
+class User(Base):
+    __tablename__ = 'Users'
+
+    idUser = Column(Integer, primary_key=True, unique=True)
+    phone = Column(String(20), nullable=False)
+    name = Column(String(40), nullable=False)
+    position = Column(String(30))
+    mail = Column(String(50), nullable=False)
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('user_id_seq'::regclass)"))
+    name = Column(String(80))
+    email = Column(String(120), unique=True)
