b449c86600a9124cc37fbc4fffc342fcf6e18e27
=joejoe <=jozefchmelar@gmail.com>
2016-03-03 10:11:48 +0100

    adding person is now working

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import BigInteger, Column, ForeignKey, Integer, String, Table, Text, text, func
 from sqlalchemy.orm import relationship
+import re
 from sqlalchemy.ext.declarative import declarative_base
 # from app.models import Company
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY','this_should_be_configured')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',
+                                          'this_should_be_configured')
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nqmuwoyhdwrxjp:DllrZMcqqxw5q_swBcQQGo1G2l@ec2-54-247-170-228.eu-west-1.compute.amazonaws.com:5432/dfuidc2lc8ohah'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgres://nqmuwoyhdwrxjp:DllrZMcqqxw5q_swBcQQGo1G2l@ec2-54-247-170-228.eu-west-1.compute.amazonaws.com:5432/dfuidc2lc8ohah'
 
 db = SQLAlchemy(app)
 
@@ -26,6 +29,24 @@ class Company(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.name
 
+
+class User(db.Model):
+    __tablename__ = 'Users'
+
+    idUser = db.Column(Integer, primary_key=True, unique=True)     
+    phone = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(40), nullable=False)
+    position = db.Column(db.String(30))
+    mail = db.Column(db.String(50), nullable=False, unique=True)
+    def __init__(self, name, phone, mail, position):
+        self.name = name.lower()
+        self.phone = phone
+        self.mail = mail.lower()
+        self.position = position.lower()
+
+    def __repr__(self):
+        return '<mail %r>' % self.mail
+
 ###
 # Routing for your application.
 ###
@@ -41,22 +62,41 @@ def home():
 # def company():
 #     """Render website's home page."""
 #     return render_template('company.html')
+@app.route('/person/', methods=['GET', 'POST'])
+def person():
+    if request.method == 'POST':
+        name = request.form['name']        
+        mail = request.form['mail']
+        if not re.match('[^@]+@[^@]+\.[^@]+',mail):
+            return '{"status":"mail format error"}'
+        position = request.form['position']        
+        phone = request.form['phone']
+        if not re.match('[\d+]{8,}',phone):
+            return '{"status":"phone format error"}'
+        user = User(name, phone, mail, position)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return '{"status":"true"}'
+        except Exception:
+            return '{"status":"duplicate mail"}'
+    else:
+        return render_template('person.html')
 
 
-@app.route('/company/',  methods=['GET', 'POST'])
+@app.route('/company/', methods=['GET', 'POST'])
 def company():
     if request.method == 'POST':
         companyName = request.form['companyName']
         company = Company(companyName)
         try:
-    	    db.session.add(company)
-    	    db.session.commit()    
-    	    return '{"status":"true"}'
+            db.session.add(company)
+            db.session.commit()
+            return '{"status":"true"}'
         except Exception:
-    	    return '{"status":"false"}'
-    else:   	
+            return '{"status":"false"}'
+    else:
         return render_template('company.html')
-
 
 
 @app.route('/about/')
