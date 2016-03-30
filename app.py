1506b828ff1fd7c20f26854bd1575725387234c5
=joejoe <=jozefchmelar@gmail.com>
2016-03-30 09:33:53 +0200

    used pipreqs to generate requs.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 import re
 from flask import Flask, render_template, request, redirect, url_for, jsonify,json
-# from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
-import models
+ 
 
 # from app.models import Company
 
