6e791642989c69cbd48366629470ddeb24004c85
jozefchmelar <no@way.com>
2016-04-02 11:22:27 +0200

    test

--- app/views.py
+++ app/views.py
@@ -104,7 +104,7 @@ def getPersonByMail(pMail):
 def testing():
     user = User.query.filter(User.mail.ilike('jeneva.garry0@mts.net')).first()
     projects = user.projects
-    return status(projects.Company.name)
+    return status(projects[0].Company.name)
 
 @app.route('/person/byMail/',methods=['GET','POST'])
 def getPersonByMailFromForm():
